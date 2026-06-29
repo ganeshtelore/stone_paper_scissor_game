718d8dba12cd3914582f0fa9ac0cde31bcd59eb6
ganeshtelore <ganesh.telore@indexnine.com>
2026-06-29 18:00:23 +0530

    Add score tracking to Rock Paper Scissors game

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function playGame(event) {
     event.preventDefault();
+    let userWin = 0;
+    let systemWin = 0;
     const userInput = document.forms["gameForm"]["user-input"].value.toLowerCase().trim();
     const resultElement = document.getElementById("result");
     const choiceArray = ['rock', 'paper', 'scissors'];
@@ -7,6 +9,9 @@ function playGame(event) {
     if (userInput === randomChoice) {
         resultElement.textContent = "It's a tie! Both chose " + userInput;
         document.forms["gameForm"]["user-input"].value="";
+        userWin = 0;
+        systemWin = 0;
+        document.getElementById("score").textContent = "Your Score: " + userWin + " System Score: " + systemWin;
     } else if (
         (userInput === 'rock' && randomChoice === 'scissors') ||
         (userInput === 'paper' && randomChoice === 'rock') ||
@@ -14,9 +19,13 @@ function playGame(event) {
     ) {
         resultElement.textContent = "You win! " + userInput + " beats " + randomChoice;
         document.forms["gameForm"]["user-input"].value="";
+        userWin+=1;
+        document.getElementById("score").textContent = "Your Score: " + userWin + " System Score: " + systemWin;
     } else if (choiceArray.includes(userInput)) {
         resultElement.textContent = "You lose! " + randomChoice + " beats " + userInput;
         document.forms["gameForm"]["user-input"].value="";
+        systemWin+=1;
+        document.getElementById("score").textContent = "Your Score: " + userWin + " System Score: " + systemWin;
     } else {
         resultElement.textContent = "Invalid input! Please enter rock, paper, or scissors.";
         document.forms["gameForm"]["user-input"].value="";
